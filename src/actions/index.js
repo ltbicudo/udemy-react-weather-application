889258d5eed4c5c356f2c61f7b1872feb49406cf
Lucas Bicudo <Lucas.Bicudo@techsolcom.local>
2017-02-22 16:37:28 -0500

    Lecture 57 to 61

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_INDEx = 'b1b15e88fa797225412429c1c50c122a1';
+import axios from 'axios';
+
+const API_KEY = '7ed57742b812cd1d38e08047c22af8fe';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
